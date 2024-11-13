06a5737b6858fc49300491e00a133ba71d215b85
Lemuel Francisco <lemuelrfrancisco@gmail.com>
2024-11-13 23:19:02 +0800

    Sample movie home page

--- client-movieproject/src/App.js
+++ client-movieproject/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './pages/Main/Main';
+import Home from './pages/Main/Movie/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ client-movieproject/src/pages/Main/Movie/Home/Home.jsx
@@ -0,0 +1,73 @@
+import { useNavigate } from 'react-router-dom';
+import './Home.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import MovieCards from '../../../../components/MovieCards/MovieCards';
+const Home = () => {
+  const accessToken = localStorage.getItem('accessToken');
+  const navigate = useNavigate();
+  const [lists, setLists] = useState([]);
+  const [featuredMovie, setFeaturedMovie] = useState(null);
+
+  const getMovies = () => {
+    //get the movies from the api or database
+    axios
+      .get('/movies')
+      .then((response) => {
+        setLists(response.data);
+        const random = Math.floor(Math.random() * response.data.length);
+        setFeaturedMovie(response.data[random]);
+      })
+      .catch((e) => console.log(e));
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (lists.length) {
+        console.log('change movie');
+        const random = Math.floor(Math.random() * lists.length);
+        setFeaturedMovie(lists[random]);
+      }
+    }, 5000);
+  }, [featuredMovie]);
+
+  return (
+    <div className='main-container'>
+      <h1 className='page-title'>Movies</h1>
+      {featuredMovie && lists.length ? (
+        <div className='featured-list-container'>
+          <div
+            className='featured-backdrop'
+            style={{
+              background: `url(${
+                featuredMovie.backdropPath !==
+                'https://image.tmdb.org/t/p/original/undefined'
+                  ? featuredMovie.backdropPath
+                  : featuredMovie.posterPath
+              }) no-repeat center top`,
+            }}
+          >
+            <span className='featured-movie-title'>{featuredMovie.title}</span>
+          </div>
+        </div>
+      ) : (
+        <div className='featured-list-container-loader'></div>
+      )}
+      <div className='list-container'>
+        {lists.map((movie) => (
+          <>
+            <MovieCards
+              movie={movie}
+              onClick={() => alert('Open modal to view Movie')}
+            />
+          </>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
